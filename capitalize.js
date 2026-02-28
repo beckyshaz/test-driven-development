de7add00d07690df93af85093354e54c761416d3
beckyshaz <sharonsakah8@gmail.com>
2026-02-28 11:16:05 +0300

    capitalizing the first letter of a string

--- capitalize.js
+++ capitalize.js
@@ -1,6 +1,13 @@
 export  function capitalize(string) {
 
+    if (!string) {
+        return;
+    }
+
     const slicedString = string.slice(1);
 
     return `${string.charAt(0).toUpperCase() + slicedString}`;
 }
+
+
+console.log(capitalize());
--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,4 @@ import { capitalize } from "./capitalize";
 test('first character capitalized', () => {
     expect(capitalize("sharon")).toBe("Sharon");
 })
+
